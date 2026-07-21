0d34733d21c183a579576db026d47778c5dc6bda
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-07-21 13:35:35 -0500

    Created a menuItems class to create multiple items
    Instead of building them all manually one by one, my plan is to use a class to build out the items as objects, then use the objects when making the DOM elements.

--- src/menu.js
+++ src/menu.js
@@ -2,33 +2,62 @@ const menuContainer = document.querySelector("#content");
 
 // First Menu Item //
 // Creates div //
-const firstMenuItemContainer = document.createElement("div");
-menuContainer.appendChild(firstMenuItemContainer);
+// const firstMenuItemContainer = document.createElement("div");
+// menuContainer.appendChild(firstMenuItemContainer);
 
-// Header //
-const classicButterHeader = document.createElement("h1");
-classicButterHeader.textContent = "Classic Butter";
-firstMenuItemContainer.appendChild(classicButterHeader);
+// // Header //
+// const classicButterHeader = document.createElement("h1");
+// classicButterHeader.textContent = "Classic Butter";
+// firstMenuItemContainer.appendChild(classicButterHeader);
 
-// Figure container for Classic Butter image //
-const classicButterImageContainer = document.createElement("figure");
-firstMenuItemContainer.appendChild(classicButterImageContainer);
+// // Figure container for Classic Butter image //
+// const classicButterImageContainer = document.createElement("figure");
+// firstMenuItemContainer.appendChild(classicButterImageContainer);
 
-// Classic Butter image and image properties //
+// // Classic Butter image and image properties //
+// import classicButterImg from "./assets/classic-butter.jpg";
+// const image1 = document.createElement("img");
+// image1.src = classicButterImg;
+// image1.alt = "A small bucket of deliciously buttery popcorn."
+// image1.width = 200; 
+// classicButterImageContainer.appendChild(image1);
+
+// // Classic Butter image caption //
+// const classicButterCaption = document.createElement("figcaption");
+// classicButterCaption.textContent = "Credit to Shameel Mukkath on Pexels.com";
+// classicButterImageContainer.appendChild(classicButterCaption);
+
+// // Classic Butter description //
+// const classicButterDesc = document.createElement("p");
+// classicButterDesc.textContent = "A fan favorite! Enjoy a timeless flavor loved by all."
+// firstMenuItemContainer.appendChild(classicButterDesc);
+
 import classicButterImg from "./assets/classic-butter.jpg";
-const image1 = document.createElement("img");
-image1.src = classicButterImg;
-image1.alt = "A small bucket of deliciously buttery popcorn."
-image1.width = 200; 
-classicButterImageContainer.appendChild(image1);
+import cheddarImg from "./assets/cheddar.jpg";
+import caramelImg from "./assets/caramel.jpg";
 
-// Classic Butter image caption //
-const classicButterCaption = document.createElement("figcaption");
-classicButterCaption.textContent = "Credit to Shameel Mukkath on Pexels.com";
-classicButterImageContainer.appendChild(classicButterCaption);
 
-// Classic Butter description //
-const classicButterDesc = document.createElement("p");
-classicButterDesc.textContent = "A fan favorite! Enjoy a timeless flavor loved by all."
-firstMenuItemContainer.appendChild(classicButterDesc);
+const menuItems = [];
 
+class menuItem {
+    constructor(name, image, caption, description) {
+        this.name = name;
+        this.image = image;
+        this.caption = caption;
+        this.description = description;
+        this.smallSize = "$6.99";
+        this.mediumSize = "$10.99";
+        this.largeSize = "$15.99";
+    }
+}
+
+const classicButter = new menuItem("Classic Butter", classicButterImg, "Credit to Shameel Mukkath on Pexels.com", "A fan favorite! Enjoy a timeless flavor loved by all.");
+
+const cheddar = new menuItem("Cheddar", cheddarImg, "Credit to Terrance Barksdale on Pexels.com", "Every bite is a blast of cheesy goodness. Grab a bag today!");
+
+const caramel = new menuItem("Caramel Corn", caramelImg, "Credit to Mikhail Nilov on Pexels.com", "Look no further for a sweet and crunchy snack crafted with love!");
+
+console.log(cheddar.smallSize);
+
+menuItems.push(classicButter, cheddar, );
+console.log(menuItems);
