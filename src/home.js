5edae27017988110f7eed76904965e886e541d9a
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-07-24 16:18:40 -0500

    Put code into a function for use in index.js
    The home page will be created when the function is run, which will occur on button-click when imported to index.js.

--- src/home.js
+++ src/home.js
@@ -1,32 +1,33 @@
-export const homeContainer = document.querySelector("#content");
+import restaurantImage from "./assets/popcorn-cat.jpg";
 
-// H1 //
-const heading = document.createElement("h1");
-heading.textContent = "Popcorn & Paws";
-homeContainer.appendChild(heading);
+export function createHomePage() {
+    const homeContainer = document.querySelector("#content");
+    const heading = document.createElement("h1");
+    heading.textContent = "Popcorn & Paws";
+    homeContainer.appendChild(heading);
 
-// Figure container for image and caption //
-const imageContainer = document.createElement("figure");
-homeContainer.appendChild(imageContainer);
+    // Figure container for image and caption //
+    const imageContainer = document.createElement("figure");
+    homeContainer.appendChild(imageContainer);
 
-// Image and image properties //
-import restaurantImage from "./assets/pexels-roman-odintsov-6422055.jpg";
-const image = document.createElement("img");
-image.src = restaurantImage;
-image.alt = "A curious calico cat investigates a spread of honey and popcorn on a sunny outdoor ledge."
-image.height = 300; 
-imageContainer.appendChild(image);
+    // Image and image properties //
+    const image = document.createElement("img");
+    image.src = restaurantImage;
+    image.alt = "A curious calico cat investigates a spread of honey and popcorn on a sunny outdoor ledge."
+    image.height = 300; 
+    imageContainer.appendChild(image);
 
-// Image caption //
-const imageCaption = document.createElement("figcaption");
-imageCaption.textContent = "Credit to Roman Odintsov on Pexels.com";
-imageContainer.appendChild(imageCaption);
+    // Image caption //
+    const imageCaption = document.createElement("figcaption");
+    imageCaption.textContent = "Credit to Roman Odintsov on Pexels.com";
+    imageContainer.appendChild(imageCaption);
 
-// P1 & P2 //
-const welcome = document.createElement("p");
-welcome.textContent = "Welcome to Popcorn & Paws, your local gourmet popcorn shop! Experience an imaginative array of salty and sweet flavors all under one roof."
-homeContainer.appendChild(welcome);
+    // P1 & P2 //
+    const welcome = document.createElement("p");
+    welcome.textContent = "Welcome to Popcorn & Paws, your local gourmet popcorn shop! Experience an imaginative array of salty and sweet flavors all under one roof."
+    homeContainer.appendChild(welcome);
 
-const proceeds = document.createElement("p");
-proceeds.textContent = "All proceeds are donated to our dedicated partner, Kam's Animal Shelter, to help animals in need find their forever homes."
-homeContainer.appendChild(proceeds);
+    const proceeds = document.createElement("p");
+    proceeds.textContent = "All proceeds are donated to our dedicated partner, Kam's Animal Shelter, to help animals in need find their forever homes."
+    homeContainer.appendChild(proceeds);
+}
