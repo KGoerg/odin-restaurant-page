ba1daa6fc648b1e782345c1f1e53f3fd51eac1d3
kgoerg <224106412+KGoerg@users.noreply.github.com>
2026-07-28 10:46:05 -0500

    Adjust dog image size

--- src/about.js
+++ src/about.js
@@ -10,13 +10,13 @@ export function createAboutPage() {
     container.appendChild(pageHeader);
 
     // About text //
-
     const aboutText1 = document.createElement("p");
     aboutText1.textContent = "At Popcorn & Paws, we use only the freshest, locally-sourced ingredients to create our outstanding popcorn flavors. Founded in 2026 by Katie Grant, Popcorn & Paws creates delectable gourmet popcorn with a heartfelt cause. We partnered with Kam's Animal Shelter to donate half of our proceeds to their operations, where they rescue and care for abandoned furry friends looking for a new, comfortable life."
     
     const aboutText2 = document.createElement("p");
     aboutText2.textContent = "At the time of writing, we are workshopping new flavors to add to our menu, so please check back in soon to see what we've come up with!"
 
+    // Append containers and text //
     container.appendChild(aboutText1);
     // Figure container for cat image and caption //
     const catImageContainer = document.createElement("figure");
@@ -42,11 +42,11 @@ export function createAboutPage() {
     const shelterDogsImage = document.createElement("img");
     shelterDogsImage.src = shelterDogs;
     shelterDogsImage.alt = "Two young pups look hopefully out of their kennel, dreaming of being adopted."
-    shelterDogsImage.height = 300; 
+    shelterDogsImage.height = 400; 
     dogImageContainer.appendChild(shelterDogsImage);
 
     // Dog image captions //
     const dogImageCaption = document.createElement("figcaption");
-    dogImageCaption.textContent = 'Puppy sisters Peanut and Cashew give hopeful looks out their kennel at potential adopters.'
+    dogImageCaption.textContent = 'Puppy sisters Peanut and Cashew give hopeful looks out their kennel at potential adopters. (Credit to Alin Luna on Pexels.com'
     dogImageContainer.appendChild(dogImageCaption);
 }
